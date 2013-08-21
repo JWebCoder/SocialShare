219eec68802100a4e030b887278012564252db85
darksier <joao87moura@gmail.com>
2013-08-21 13:33:20 +0100

    New Social Networks
    Added:
    google bookmarks
    tumblr
    delicious
    reddit
    tapiture
    stumbleupon
    newsvine

--- SocialShare.js
+++ SocialShare.js
@@ -1,3 +1,11 @@
+//	Author: João Moura
+//	Author blog: http://JWebCoder.wordpress.com
+//
+//	SocialShare v0.8
+//
+//	JavaScript plugin to create sharing buttons
+//
+
 var SocialShare = function() {
     return this;
 };
@@ -20,16 +28,53 @@ SocialShare.prototype = {
 		this.type = "linkedin";
 		this.publish();
 	},
-	google: function(url) {
+	googleplus: function(url) {
 		this.link = "https://plus.google.com/share?url=" + url;
-		this.type = "google";
+		this.type = "googleplus";
 		this.publish();
 	},
+	googlebookmark: function(url,title,summary) {
+		this.link = "http://www.google.com/bookmarks/mark?op=edit&bkmk=" + url + "&title=" + title + "&annotation=" + summary;
+		this.type = "googlebookmark";
+		this.publish();
+	},
+	
 	pinterest: function(url,image,summary) {
 		this.link = "http://pinterest.com/pin/create/button/?url=" + url + "&media=" + image + "&description=" + summary;
 		this.type = "pinterest";
 		this.publish();
 	},
+	tumblr: function(url,title,summary) {
+		this.link = "http://www.tumblr.com/share/link?url=" + url + "&name=" + title + "&description=" + summary;
+		this.type = "tumblr";
+		this.publish();
+	},
+	delicious: function(url,title,summary) {
+		this.link = "http://delicious.com/post?url=" + url + "&title=" + title + "&notes=" + summary;
+		this.type = "delicious";
+		this.publish();
+	},
+	reddit: function(url,title) {
+		this.link = "http://www.reddit.com/submit?url=" + url + "&title=" + title;
+		this.type = "reddit";
+		this.publish();
+	},
+	tapiture: function(url,title,imgUrl,imgTitle,width,height) {
+		this.link = "http://tapiture.com/bookmarklet/image?img_src=" + imgUrl + "&page_url=" + url + "&page_title=" + title + "&img_title=" + imgTitle + "&img_width=" + width + "img_height=" + height;
+		this.type = "tapiture";
+		this.publish();
+	},
+	stumbleupon: function(url,title) {
+		this.link = "http://www.stumbleupon.com/submit?url=" + url + "&title=" + title;
+		this.type = "stumbleupon";
+		this.publish();
+	},
+	newsvine: function(url,title) {
+		this.link = "http://www.newsvine.com/_tools/seed&save?u=" + url + "&h=" + title;
+		this.type = "newsvine";
+		this.publish();
+	},
+	
 	share: function(element) {
 		if(element.getAttribute("data-type") == "facebook"){
 			url = element.getAttribute("data-url");
@@ -46,22 +91,58 @@ SocialShare.prototype = {
 			title = element.getAttribute("data-title");
 			summary = element.getAttribute("data-summary");
 			this.linkedin(url,title,summary);
-		}else if(element.getAttribute("data-type") == "google"){
+		}else if(element.getAttribute("data-type") == "googleplus"){
 			url = element.getAttribute("data-url");
-			this.google(url);
+			this.googleplus(url);
+		}else if(element.getAttribute("data-type") == "googlebookmark"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			summary = element.getAttribute("data-summary");
+			this.googlebookmark(url,title,summary);
 		}else if(element.getAttribute("data-type") == "pinterest"){
 			url = element.getAttribute("data-url");
 			image = element.getAttribute("data-image");
 			summary = element.getAttribute("data-summary");
 			this.pinterest(url,image,summary);
+		}else if(element.getAttribute("data-type") == "tumblr"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			summary = element.getAttribute("data-summary");
+			this.tumblr(url,title,summary);
+		}else if(element.getAttribute("data-type") == "delicious"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			summary = element.getAttribute("data-summary");
+			this.delicious(url,title,summary);
+		}else if(element.getAttribute("data-type") == "reddit"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			this.reddit(url,title);
+		}else if(element.getAttribute("data-type") == "tapiture"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			imgUrl = element.getAttribute("data-image");
+			imgTitle = element.getAttribute("data-image-title");
+			width = element.getAttribute("data-image-width");
+			height = element.getAttribute("data-image-height");
+			this.tapiture(url,title,imgUrl,imgTitle,width,height);
+		}else if(element.getAttribute("data-type") == "stumbleupon"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			this.stumbleupon(url,title);
+		}else if(element.getAttribute("data-type") == "newsvine"){
+			url = element.getAttribute("data-url");
+			title = element.getAttribute("data-title");
+			this.newsvine(url,title);
 		}
 	},
+	
 	publish: function() {
 		if(this.link != ""){
 			$params = "";
-			if(this.type != "google"){
+			if(this.type != "googleplus"){
 				$params = "toolbar=0,status=0,width=626,height=436";
-			}else if (this.type == "google"){
+			}else if (this.type == "googleplus"){
 				$params = "toolbar=0,status=0,width=600,height=600";
 			}
 			window.open(this.link.replace(/(<([^>]+)>)/ig, ""), 'sharer',$params);
